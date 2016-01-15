493b133182eed34e10867f06c01b0286147bbcae
Kyle Barton <kbarton@incomm.com>
2016-01-15 10:28:24 -0800

    Working on that persistence layer. Got mongodb set up running as a service. Need to update the readme with instructions on how to do that. I'll set it up on my mac first and then work from there.

--- DataAccess/drinkRepository.js
+++ DataAccess/drinkRepository.js
@@ -5,6 +5,19 @@ var drinkRepository = {
 		return drinkId;
 	},
 	addDrink: function(drink){
+		var mongoClient = require('mongodb').MongoClient;
+		var url = 'mongodb://localhost:27017/CoffeeApi';
+		mongoClient.connect(url, function(err, db){
+			if (err){
+				throw 'Could not connect to mongo database';
+			}
+			console.log(drink);
+			console.log('successfully connected.');
+			db.collection('Drinks').save(drink, function(err, record){
+				console.log(record);
+			});
+			db.close();
+		})
 		return {"drink": drink};
 	},
 	changeDrink: function(drinkId, drink){
@@ -18,7 +31,18 @@ var drinkRepository = {
 	},
 	deleteAllDrinks: function(){
 		return "deleting all drinks!";
-	}
+	},
+	// persist: function(cmd){
+	// 	var mongoClient = require('mongodb').MongoClient;
+	// 	var url = 'mongodb://localhost:27017/CoffeeApi';
+	// 	mongoClient.connect(url, function(err, db){
+	// 		if (err){
+	// 			throw 'Could not connect to mongo database';
+	// 		}
+	// 		console.log('successfully connected. Our command is ' + cmd);
+	// 		db.close();
+	// 	})
+	// }
 }
 
 
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 //take care of my requires
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var drinkModel = require('./Models/drink.js')
 var drinksControllerProto = require('./Controllers/drinksController.js');
 //init my server
 var app = express();
+app.use(bodyParser.json());
 //init my controllers
 var drinksController = Object.create(drinksControllerProto);
 
 //for testing, will turn this off soon.
 app.get('/', function(req, res){ //turn this into a route guide
 	res.send('hello');
-})
+});
 
 //set up uris
 app.listen(3000);
@@ -25,28 +28,47 @@ var ordersEndpoint = "/orders";
 
 //drinks with no uri
 app.route(drinksEndpoint)
+
 .get(function(req, res){
+
 	res.send(drinksController.getDrinks());
+
 })
+
 .post(function(req, res){
-	console.log(req);
-	// console.log(req.body);
-	// var drink = Object.create(drinkModel);
-	// drink.init(req.body);
-	// res.send(drinksController.addADrink(req.body));
+
+	var drink = Object.create(drinkModel);
+	drink.init(req.body);
+
+	res.send(drinksController.addADrink(drink));
+
 })
+
 .delete(function(req, res){
+
 	res.send(drinksController.deleteDrinks());
+
 });
 
 //drinks with uri
 app.route(drinksEndpoint+"/:drinkId")
+
 .get(function(req, res){
+
 	res.send(drinksController.getADrink(req.params.drinkId));
+
 })
+
 .put(function(req, res){
-	res.send(drinksController.updateADrink(req.params.drinkId, req.body));
+
+	var drink = Object.create(drinkModel);
+	drink.init(req.body);
+
+	res.send(drinksController.updateADrink(req.params.drinkId, drink));
 })
+
 .delete(function(req, res){
+
 	res.send(drinksController.deleteADrink(req.params.drinkId));
+
 })
